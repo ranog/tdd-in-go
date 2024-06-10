07dc6ea76b0f3e18648e5f030798329898a99324
Joao.Nogueira <jprnogueira@yahoo.com.br>
2024-06-09 22:36:35 -0300

    Benchmarks are an important Go profiling tool

--- chapter02/calculator/engine.go
+++ chapter02/calculator/engine.go
@@ -1,24 +1,7 @@
 package calculator
 
-import "fmt"
-
 type Engine struct{}
 
 func (e *Engine) Add(x, y float64) float64 {
 	return x + y
-}
-
-func (e *Engine) Subtract(x, y float64) float64 {
-	return x - y
-}
-
-func (e *Engine) Multiply(x, y float64) float64 {
-	return x * y
-}
-
-func (e *Engine) Divide(x, y float64) (float64, error) {
-	if y == 0 {
-		return 0, fmt.Errorf("division by zero")
-	}
-	return x / y, nil
 }
--- chapter02/calculator/engine_test.go
+++ chapter02/calculator/engine_test.go
@@ -52,49 +52,9 @@ func TestAdd(t *testing.T) {
 	})
 }
 
-func TestSubtract(t *testing.T) {
-	x, y := 2.5, 3.5
-	want := x - y
+func BenchmarkAdd(b *testing.B) {
 	e := calculator.Engine{}
-
-	got := e.Subtract(x, y)
-
-	if got != want {
-		t.Errorf("Subtract(%.2f, %.2f) incorrect, got: %.2f, want: %.2f", x, y, got, want)
-	}
-}
-
-func TestMultiply(t *testing.T) {
-	x, y := 2.5, 3.5
-	want := x * y
-	e := calculator.Engine{}
-
-	got := e.Multiply(x, y)
-
-	if got != want {
-		t.Errorf("Multiply(%.2f, %.2f) incorrect, got: %.2f, want: %.2f", x, y, got, want)
-	}
-}
-
-func TestDivide(t *testing.T) {
-	x, y := 2.5, 3.5
-	want := x / y
-	e := calculator.Engine{}
-
-	got, _ := e.Divide(x, y)
-
-	if got != want {
-		t.Errorf("Divide(%.2f, %.2f) incorrect, got: %.2f, want: %.2f", x, y, got, want)
-	}
-}
-
-func TestDivideByZero(t *testing.T) {
-	x, y := 2.5, 0.0
-	e := calculator.Engine{}
-
-	_, err := e.Divide(x, y)
-
-	if err == nil {
-		t.Error("Division by zero should return an error")
+	for i := 0; i < b.N; i++ {
+		e.Add(2.5, 3.5)
 	}
 }
